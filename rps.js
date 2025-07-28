0e941108153d88156ee0aeb45979b564e86a8cbe
guido <guido@agatha>
2025-07-28 14:11:57 -0300

    Adds computer choice

---
+++ rps.js
@@ -0,0 +1,10 @@
+function getComputerChoice(){
+    val = Math.random();
+    if (val < 1/3){
+        return "rock";
+    }else if (val >=1/3 && val <2/3){
+        return  "paper"
+    }else {
+        return "scissors"
+    }
+}
