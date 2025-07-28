b1ac3ae0f2f5642694d21676344b36066f460d87
guido <guido@agatha>
2025-07-28 16:46:23 -0300

    Implements single round logic

--- rps.js
+++ rps.js
@@ -13,3 +13,42 @@ function getHumanChoice(){
     let choice = prompt("RPS?")
     return choice.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    if (humanChoice == computerChoice){
+        console.log("Tied.")
+    } else {
+        if (humanChoice == "rock"){
+            if (computerChoice == "paper"){
+                console.log(`You chose ${humanChoice} and the computer chose ${computerChoice} so you lose`);
+                computerScore ++;
+            } else {
+                console.log(`You chose ${humanChoice} and the computer chose ${computerChoice} so you win`);
+                humanScore ++;
+            }
+        } else if (humanChoice == "paper") {
+            if (computerChoice == "scissors"){
+                console.log(`You chose ${humanChoice} and the computer chose ${computerChoice} so you lose`);
+                computerScore ++;
+            } else {
+                console.log(`You chose ${humanChoice} and the computer chose ${computerChoice} so you win`);
+                humanScore ++;
+            }
+        } else if (humanChoice == "scissors") {
+            if (computerChoice == "rock"){
+                console.log(`You chose ${humanChoice} and the computer chose ${computerChoice} so you lose`);
+                computerScore ++;
+            } else {
+                console.log(`You chose ${humanChoice} and the computer chose ${computerChoice} so you win`);
+                humanScore ++;
+            }
+        }
+    }
+}
+
+playRound();
